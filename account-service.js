a980f762bdcada7e4a07ed1b8f7943a7e934fa81
hkholghi <hkholghi@abingtonfriends.net>
2019-09-24 13:20:26 -0400

    Added a statement This. . . to the utility file

--- account-service.js
+++ account-service.js
@@ -1,8 +1,8 @@
 const sum = require('./utility.js')
 
 let checkingAccountBalance = 200
-let savingAccountBalance = 100
-let retirementAccountBalance = 2000
+let savingAccountBalance = 1000
+let retirementAccountBalance = 20000
 
 let totalBalance = sum([checkingAccountBalance, savingAccountBalance, retirementAccountBalance])
 console.log(totalBalance)
