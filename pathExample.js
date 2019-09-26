88b5f7ac5bd1da8957210eba5ab23950b0a4485f
Hkholghi <hkholghi@abingtonfriends.net>
2019-09-26 07:55:19 -0400

    'update'

--- pathExample.js
+++ pathExample.js
@@ -1,4 +1,9 @@
 const path = require('path')
-console.log(path)
-const server = require(path.join('app','server.js'))
-console.log(server);
+//console.log(path)
+let relativeAddressing = path.join('Users', 'Refsnes', 'demo_path.js')
+//const server = require(path.join('app','server.js'))
+
+console.log(relativeAddressing)
+
+let absoulteAddressing = path.join(__dirname,'app','server.js')
+console.log(absoulteAddressing)
